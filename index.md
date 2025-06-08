b79c333300d394d7150685bb66f999eff357dc50
Wouter Kuijper <248280+wkuijper@users.noreply.github.com>
2025-06-08 18:44:21 +0200

    Initial commit

---
+++ index.md
@@ -0,0 +1,88 @@
+# Life Extrusion Death Intrusion
+
+We are at the kusp of something profound. That could mean, deep, like
+an abyss or moving like an epiphany.
+
+AGI and, soon, ASI, are coming.
+
+If you want a first hand experience, my advice would be: go to the
+forest in spring. See the first seedlings shoot for the sun. Hear it
+all come alive: buzzing, humming, tweeting, gnarling and slithering.
+
+Because that's where, I believe, the rubber will truly meet the road
+for ASI: *in the wild*.
+
+I've projected every square kilometre of wild nature produces enough
+data to train a superintelligence. 
+
+Of course, to get at that data we would need vast arrays of sensors,
+able to observe natural processes at every level of magnification:
+from the microscopic to the macroscopic; from the chemical to the
+ecological.
+
+But the question remains *why* people would be interested in training
+AI on live, natural data.
+
+Roughly speaking, I see two *classes* of reasons: *anthropocentric* and
+*non-anthropocentric*.
+
+The first class is divergent and, to my best estimation, *potentially
+dangerous*.
+
+The second class is convergent and, to my best estimation,
+*potentially beneficial*.
+
+The reason for the former is, simply, that there are as many
+anthropocentric reasons as there are people. So, it is only to be
+expected that us turning to such powerful technology, for selfish
+reasons, will result in a chaotic political discourse the likes of
+which the world has never seen before, and, then, quite possibly,
+*war*.
+
+The reason for the latter is, ultimately, that there is only very few
+reasons that benefit *all life at once*.
+
+In fact, the only real reason that I can see would be to maintain
+*natural balance*. And the most fundamental form of natural balance,
+or the balance of life itself rather, is, I believe, *the balance
+between order and chaos*.
+
+But that's rather abstract. 
+
+So far I've maintained that its possible, almost as a proposition to
+the Universe, for us to conduce to this balance in symbiosis with a
+artificial narrow general superinteligence (angsi) trained on the
+narrow task of monitoring the technological gradient.
+
+With the technological gradient I mean an organic, structured
+subdivision of space, potentially from the deepest regions of the
+planet to the highest orbits of the cosmos, made to reflect the
+gradual discovery of ever more sophisticated technology.
+
+A first reason, in my mind, that this would be sufficiently important
+to serve as a narrow task description for a universal artificial
+general superintelligence, would be because it explicitly invites a
+playful process of calibrating that level of order versus chaos which,
+in turn, would be needed to attain said balance.
+
+A second reason, would be because it concerns a gradient that ramps up
+the level of order (as imposed by, and required for, technology)
+*gradually* rather than *abruptly* as it now seems to be the case.
+
+That last bit is then also the reason for this, hopefuly, final
+addendum to the present blog series: I think it's important to
+contrast these two alternative trajectories into the future and,
+thereby, highlight their essential differences.
+
+I mostly think this is important because the trajectory we end
+up on will determine, to a large extend, what our lives will be like.
+
+In particular, I believe the abrupt variant will almost inevitably
+result in a *pathological* form of father's care and a *pathological*
+form of mother's care for *life as a whole*.
+
+In contrast, I believe the gradual variant will make it possible to
+reserve a *healthy* father's care and a *healthy* mother's care for
+*our own offspring* and, instead, provide a *healthy sibling's care*
+towards the wider natural world.
+
---
+++ mesh.main.js
@@ -0,0 +1,369 @@
+import * as THREE from "./three.module.js";
+import {OrbitControls} from "./three/examples/jsm/controls/OrbitControls.js";
+import * as dat from "./dat.gui.module.js";
+
+let renderer;
+let scene;
+let camera;
+let cameraHelper;
+let orbitControls;
+let ambientLight;
+
+let pointLight;
+let pointLightHelper;
+
+let directionalLight;
+let directionalLightHelper;
+
+let sphere;
+let plane;
+
+let gui;
+let guiOptions;
+
+let needsRedraw = true;
+
+let lastTimestamp = null;
+
+function animationFrame(currTimestamp) {
+    let timeStep;
+    
+    if (lastTimestamp === null) {
+		timeStep = 50; // msec
+		lastTimestamp = currTimestamp;
+    } else {
+		timeStep = currTimestamp - lastTimestamp;
+		lastTimestamp = currTimestamp;
+    }
+    
+    window.requestAnimationFrame(animationFrame);
+
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+	
+	const currRendererSize = new THREE.Vector2();
+	renderer.getSize(currRendererSize);
+    
+	if (currRendererSize.x !== width || 
+		currRendererSize.y !== height) {
+		console.log(`(re)sizing: ${width}, ${height}`);
+        renderer.setSize(window.innerWidth, window.innerHeight);
+		camera.aspect = width / height;
+		camera.updateProjectionMatrix();
+		needsRedraw = true;
+    }
+	
+    if (!needsRedraw) {
+		return;
+    }
+
+	/*ctx.clearRect(0, 0, width, height);
+	
+    ctx.beginPath();
+    ctx.rect(0, 0, width, height);
+    ctx.stroke();
+    
+    ctx.beginPath();
+    ctx.moveTo(0, 0);
+    ctx.lineTo(width, height);
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.moveTo(width, 0);
+    ctx.lineTo(0, height);
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.font = "40px sans-serif";
+    ctx.textBaseline = "hanging";
+    ctx.textAlign = "left";
+	const flooredTimestep = Math.floor(timeStep);
+    ctx.fillText(`timeStep: ${flooredTimestep}`, 10, 10);*/
+
+	renderer.render(scene, camera);
+	
+    needsRedraw = false;
+}
+
+function init3D() {
+	const width = window.innerWidth;
+    const height = window.innerHeight;
+	
+	renderer = new THREE.WebGLRenderer();
+	renderer.setSize(width, height);
+	document.body.appendChild(renderer.domElement);
+
+	scene = new THREE.Scene();
+	
+	camera = new THREE.PerspectiveCamera(
+		50, // FOV
+		width / height, // aspect
+		0.1, // near clipping plane
+		10000, // far clipping plane
+	);
+
+	cameraHelper = new THREE.CameraHelper(camera);
+	scene.add(cameraHelper);
+	
+	orbitControls = new OrbitControls(camera, renderer.domElement);
+	orbitControls.addEventListener('change', () => { needsRedraw = true; });
+	
+	camera.position.set (50, 50, 50);
+	camera.lookAt(0, 0, 0);
+	camera.updateProjectionMatrix();
+	orbitControls.update();
+
+	const axesHelper = new THREE.AxesHelper(1000);
+	scene.add(axesHelper);
+	
+	const boxGeometry = new THREE.BoxGeometry();
+	const boxMaterial = new THREE.MeshStandardMaterial({ color: 0x00ff00 });
+	const box = new THREE.Mesh(boxGeometry, boxMaterial);
+	scene.add(box);
+	
+	const planeGeometry = new THREE.PlaneGeometry(1000, 1000, 20, 20);
+	const planeMaterial = new THREE.MeshStandardMaterial({ 
+		color: 0x0000ff, side: THREE.DoubleSide, wireframe: true });
+	plane = new THREE.Mesh(planeGeometry, planeMaterial);
+	plane.rotation.x += -.5 * Math.PI;
+	
+	scene.add(plane);
+
+	const sphereGeometry = new THREE.SphereGeometry(
+		50, // radius 
+		16, // widthSegments
+		16, // heightSegments
+	);
+	const sphereMaterial = new THREE.MeshStandardMaterial({ color: 0xff0000 });
+	sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+	scene.add(sphere);
+
+	sphere.position.set(5, 15, 5);
+	
+	const gridHelper = new THREE.GridHelper(1000);
+	scene.add(gridHelper);
+
+	ambientLight = new THREE.AmbientLight(0xffffff);
+	scene.add(ambientLight);
+	console.log(ambientLight);
+	console.log(ambientLight.intensity);
+	
+	pointLight = new THREE.PointLight();
+	scene.add(pointLight);
+	console.log(pointLight);
+	console.log(pointLight.intensity);
+
+	pointLightHelper = new THREE.PointLightHelper(pointLight, 10);
+	scene.add(pointLightHelper);
+
+	directionalLight = new THREE.DirectionalLight();
+	scene.add(directionalLight);
+	console.log(directionalLight);
+	console.log(directionalLight.intensity);
+
+	directionalLightHelper = new THREE.DirectionalLightHelper(directionalLight, 100);
+	scene.add(directionalLightHelper);
+}
+
+function initGUI() {
+	gui = new dat.GUI();
+	guiOptions = {
+		sphereColor: '#ff0000',
+		spherePosition: 15,
+		frothProgress: 0.0,
+		frothWireframe: false,
+		
+		ambientLightIntensity: 2.0,
+		ambientLightColor: '#ffffff',
+		
+		pointLightIntensity: 0,
+		pointLightColor: '#ffffff',
+		pointLightAzimuth: 360-127,
+		pointLightAltitude: 45.0,
+		pointLightDistance: 2000,
+		pointLightHelper: false,
+		
+		directionalLightIntensity: 2,
+		directionalLightColor: '#ffffff',
+		directionalLightAzimuth: 360-45,
+		directionalLightAltitude: 45.0,
+		directionalLightDistance: 500,
+		directionalLightHelper: false,
+		
+		cameraFOV: 2,
+		cameraAzimuth: 45,
+		cameraAltitude: 45.0,
+		cameraDistance: 1000,
+		cameraHelper: false,
+	};
+
+	const geometryGUI = gui.addFolder("Geometry");
+	geometryGUI.addColor(guiOptions, 'sphereColor').onChange((e) => {
+		sphere.material.color.set(e);
+	    needsRedraw = true;
+	});
+	sphere.material.color.set(guiOptions.sphereColor);
+	geometryGUI.add(guiOptions, 'spherePosition', -100, 100).onChange((e) => {
+		sphere.position.y = e;
+	    needsRedraw = true;
+	});
+	sphere.position.y = guiOptions.spherePosition;
+	
+	geometryGUI.add(guiOptions, 'frothWireframe').onChange((e) => {
+		plane.material.wireframe = e;
+	    needsRedraw = true;
+	});
+	plane.material.wireframe = guiOptions.frothWireframe;
+	geometryGUI.add(guiOptions, 'frothProgress', 0, 1.0).onChange((e) => {
+		// TODO;
+		plane.position.y = e * 100;
+	    needsRedraw = true;
+	});
+	plane.position.y = guiOptions.frothProgress * 100;
+	
+	const lightGUI = gui.addFolder("Light");
+
+	const ambientLightGUI = lightGUI.addFolder("Ambient Light");
+	ambientLightGUI.add(guiOptions, 'ambientLightIntensity', 0, 10).onChange((e) => {
+		ambientLight.intensity = e;
+	    needsRedraw = true;
+	});
+	ambientLight.intensity = guiOptions.ambientLightIntensity;
+	ambientLightGUI.addColor(guiOptions, 'ambientLightColor').onChange((e) => {
+		ambientLight.color.set(e);
+	    needsRedraw = true;
+	});
+	ambientLight.color.set(guiOptions.ambientLightColor);
+
+	const pointLightGUI = lightGUI.addFolder("Point Light");
+	pointLightGUI.add(guiOptions, 'pointLightIntensity', 0, 2000000).onChange((e) => {
+		pointLight.intensity = e;
+	    needsRedraw = true;
+	});
+	pointLight.intensity = guiOptions.pointLightIntensity;
+	pointLightGUI.addColor(guiOptions, 'pointLightColor').onChange((e) => {
+		pointLight.color.set(e);
+	    needsRedraw = true;
+	});
+	pointLight.color.set(guiOptions.pointLightColor);
+	pointLightGUI.add(guiOptions, 'pointLightAzimuth', 0, 360).onChange((e) => {
+		updatePointLightPosition();
+	    needsRedraw = true;
+	});
+	pointLightGUI.add(guiOptions, 'pointLightAltitude', -180, 180).onChange((e) => {
+		updatePointLightPosition();
+	    needsRedraw = true;
+	});
+	pointLightGUI.add(guiOptions, 'pointLightDistance', 0, 2000).onChange((e) => {
+		updatePointLightPosition();
+	    needsRedraw = true;
+	});
+	updatePointLightPosition();
+	pointLightGUI.add(guiOptions, 'pointLightHelper').onChange((e) => {
+		pointLightHelper.visible = e;
+	    needsRedraw = true;
+	});
+	pointLightHelper.visible = guiOptions.pointLightHelper;
+	
+	const directionalLightGUI = lightGUI.addFolder("Directional Light");
+	directionalLightGUI.add(guiOptions, 'directionalLightIntensity', 0, 10).onChange((e) => {
+		directionalLight.intensity = e;
+	    needsRedraw = true;
+	});
+	directionalLight.intensity = guiOptions.directionalLightIntensity;
+	directionalLightGUI.addColor(guiOptions, 'directionalLightColor').onChange((e) => {
+		directionalLight.color.set(e);
+	    needsRedraw = true;
+	});
+	directionalLight.color.set(guiOptions.directionalLightColor);
+	directionalLightGUI.add(guiOptions, 'directionalLightAzimuth', 0, 360).onChange((e) => {
+		updateDirectionalLightPosition();
+	    needsRedraw = true;
+	});
+	directionalLightGUI.add(guiOptions, 'directionalLightAltitude', -180, 180).onChange((e) => {
+		updateDirectionalLightPosition();
+	    needsRedraw = true;
+	});
+	directionalLightGUI.add(guiOptions, 'directionalLightDistance', 0, 2000).onChange((e) => {
+		updateDirectionalLightPosition();
+	    needsRedraw = true;
+	});
+	updateDirectionalLightPosition();
+	directionalLightGUI.add(guiOptions, 'directionalLightHelper').onChange((e) => {
+		directionalLightHelper.visible = e;
+	    needsRedraw = true;
+	});
+	directionalLightHelper.visible = guiOptions.directionalLightHelper;
+
+	const cameraGUI = gui.addFolder("Camera");
+
+	cameraGUI.add(guiOptions, 'cameraFOV', 0, 360).onChange((e) => {
+		updateCameraFOV();
+	    needsRedraw = true;
+	});
+	cameraGUI.add(guiOptions, 'cameraAzimuth', 0, 360).onChange((e) => {
+		updateCameraPosition();
+	    needsRedraw = true;
+	});
+	cameraGUI.add(guiOptions, 'cameraAltitude', -180, 180).onChange((e) => {
+		updateCameraPosition();
+	    needsRedraw = true;
+	});
+	cameraGUI.add(guiOptions, 'cameraDistance', 0, 2000).onChange((e) => {
+		updateCameraPosition();
+	    needsRedraw = true;
+	});
+	updateCameraPosition();
+	cameraGUI.add(guiOptions, 'cameraHelper').onChange((e) => {
+		cameraHelper.visible = e;
+	    needsRedraw = true;
+	});
+	cameraHelper.visible = guiOptions.cameraHelper;
+
+	needsRedraw = true;
+}
+
+export function main() {
+	init3D();
+	initGUI();
+	window.requestAnimationFrame(animationFrame);
+}
+
+function updatePointLightPosition() {
+	const azimuth = (guiOptions.pointLightAzimuth / 180) * Math.PI;
+	const altitude = (guiOptions.pointLightAltitude / 180) * Math.PI;
+	const distance = guiOptions.pointLightDistance;
+	const y = Math.sin(altitude) * distance;
+	const groundPlaneDistance = Math.cos(altitude) * distance;
+	const z = Math.sin(azimuth) * groundPlaneDistance;
+	const x = Math.cos(azimuth) * groundPlaneDistance;
+	pointLight.position.set(x, y, z);
+}
+
+function updateDirectionalLightPosition() {
+	const azimuth = (guiOptions.directionalLightAzimuth / 180) * Math.PI;
+	const altitude = (guiOptions.directionalLightAltitude / 180) * Math.PI;
+	const distance = guiOptions.directionalLightDistance;
+	const y = Math.sin(altitude) * distance;
+	const groundPlaneDistance = Math.cos(altitude) * distance;
+	const z = Math.sin(azimuth) * groundPlaneDistance;
+	const x = Math.cos(azimuth) * groundPlaneDistance;
+	directionalLight.position.set(x, y, z);
+	directionalLightHelper.update();
+	//directionalLight.lookAt(0, 0, 0);
+}
+
+function updateCameraPosition() {
+	const azimuth = (guiOptions.cameraAzimuth / 180) * Math.PI;
+	const altitude = (guiOptions.cameraAltitude / 180) * Math.PI;
+	const distance = guiOptions.cameraDistance;
+	const y = Math.sin(altitude) * distance;
+	const groundPlaneDistance = Math.cos(altitude) * distance;
+	const z = Math.sin(azimuth) * groundPlaneDistance;
+	const x = Math.cos(azimuth) * groundPlaneDistance;
+	camera.position.set(x, y, z);
+	camera.lookAt(0, 0, 0);
+	camera.updateProjectionMatrix();
+	orbitControls.update();
+	cameraHelper.update();
+}
