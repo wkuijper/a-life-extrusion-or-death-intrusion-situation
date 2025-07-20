193cea53215318ecb3be2e33438ff7e5d06637f4
Wouter Kuijper <248280+wkuijper@users.noreply.github.com>
2025-07-20 13:55:19 +0200

    Linalg

--- cavern.test.js
+++ cavern.test.js
@@ -1,6 +1,340 @@
-import { CavernGrid } from "./cavern.js";
+import { CavernGrid, HalfFace, Tetrahedron } from "./cavern.js";
+
 import * as THREE from "./three.module.js";
 
+class Net {
+
+    _reportNewNetMesh(netMesh) {
+        this.__netMeshes.push(netMesh);
+    }
+    
+    _reportNewNetFace(netFace) {
+        this.__netFaceMap(netFaceMap.halfFace, netFace);
+    }
+
+    _reportNewNetHalfEdge(netHalfEdge) {
+        this.__netHalfEdgeMap(netFaceMap.halfFace, netFace);
+    }
+    
+    constructor(initialTetrahedronsAndHalfFaces) {
+        this.__animationSteps = [];
+        this.__currentAnimationStep = null;
+        this.__totalTime = 0;
+
+        this.__finished = false;
+
+        this.__netMeshes = [];
+        this.__netFaceMap = new Map();
+        this.__netHalfEdgeMap = new Map();
+        
+        this.__currHalfFaceColor = new Map();
+        this.__currHalfEdgeTranslation = new Map();
+    }
+
+    animationStep(time) {
+        const animationStep = new AnimationStep(this, this.__animationSteps.length, this.__currentAnimationStep, time)
+        this.__animationSteps.push(animationStep);
+        this.__currentAnimationStep = animationStep;
+        this.__totalTime += time;
+        return animationStep;
+    }
+
+    finish() {
+        
+        this.__finished = true;
+    }
+}
+
+class _NetMesh {
+
+    _reportNewNetFace(netFace) {
+        netFace._index = this.__netFaceIndex;
+        this.__netFaces[this.__netFaceIndex] = netFace;
+        this.__netFaceIndex++;
+        this.__net._reportNewNetFace(netFace);
+    }
+    
+    _reportNewNetHalfEdge(netHalfEdge) {
+        netFace._index = this.__netFaceIndex;
+        this.__netFaces[this.__netFaceIndex] = netFace;
+        this.__netFaceIndex++;
+        this.__net._reportNewNetHalfEdge(netHalfEdge);
+    }
+    
+    constructor(net, halfFaceSet) {
+        this.__halfFaceSet = halfFaceSet;
+        faceIndex = 0;
+        this.__netFaceIndex = 0;
+        this.__netFaces = new Array(halfFaceSet.size);
+
+        let totX = 0;
+        let totY = 0;
+        let totZ = 0;
+        let totCount = 0;
+        
+        for (const halfFace of halfFaceSet) {
+            for (const halfEdge of halfFace) {
+                const sourceVertex = halfEdge.sourceVertex;
+                totX += sourceVertex.x;
+                totY += sourceVertex.y;
+                totZ += sourceVertex.z;
+                totCount++;
+            }
+        }
+
+        const posX = totCount === 0 ? 0 : totX / totCount;
+        const posY = totCount === 0 ? 0 : totY / totCount;
+        const posZ = totCount === 0 ? 0 : totZ / totCount;
+        
+        this.__posX = posX;
+        this.__posY = posY;
+        this.__posZ = posZ;
+
+        this.__rotA = 1;
+        this.__rotB = 0;
+        this.__rotC = 0;
+        this.__rotD = 0;
+        this.__rotE = 1;
+        this.__rotF = 0;
+        this.__rotG = 0;
+        this.__rotH = 0;
+        this.__rotI = 1;
+        
+        for (const halfFace of halfFaceSet) {
+            const netFace = new _NetFace(netMesh, halfFace);
+        }
+        
+        net._reportNewNetMesh(this);
+    }
+    
+}
+
+class _NetFace {
+
+    constructor(netMesh, halfFace) {
+        this.__netMesh = netMesh;
+        this._index = -1;
+        this.__halfFace = halfFace;
+        const [he1, he2, he3] = halfFace.halfEdges();
+        this.__netHalfEdge1 = new _NetHalfEdge(net, this, he1);
+        this.__netHalfEdge2 = new _NetHalfEdge(net, this, he2);
+        this.__netHalfEdge3 = new _NetHalfEdge(net, this, he3);
+        netMesh._reportNetFace(this);
+    }
+    
+}
+
+class _NetHalfEdge {
+
+    constructor(net, netFace, halfEdge) {
+        this.__netFace = netFace;
+        this._index = -1;
+        this.__halfEdge = halfEdge;
+        const sourceVertex = halfEdge.sourceVertex;
+        const absX = sourceVertex.x;
+        this.__absX = absX;
+        this.__relX = netFace.
+        this.__absY = sourceVertex.y;
+        this.__absZ = sourceVertex.z;
+        this.__relX = ;
+        net._reportNetHalfEdge(this);
+    }
+
+    absPositionAsVec3() {
+        return [this.__x, this.__y, this.__z];
+    }
+
+    relPositionAsVec3() {
+        return [this.__x, this.__y, this.__z];
+    }
+}
+
+class AnimationStep {
+
+    constructor(parentNet, index, previousStep) {
+        this.__parentNet = parentNet;
+        this.__index = index;
+        this.__previousStep = previousStep;
+        this._nextStep = null;
+        
+        if (previousStep !== null) {
+            previousStep._nextStep = this;
+        }
+        
+        this.__changes = [];
+        this.__changedHalfFaces = new Map();
+    }
+    
+    color(tetrahedronsAndHalfFaces, color) {
+        const coloring = new Coloring(this, this.__changes.length, tetrahedronsAndHalfFaces, color);
+        this._treatNewChange(coloring);
+        return this;
+    }
+
+    unfold(tetrahedronsAndHalfFaces, halfEdge, angle) {
+        const unfolding = new Unfolding(this, this.__changes.length, tetrahedronsAndHalfFaces, halfEdge, angle);
+        this._treatNewChange(unfolding);
+        return this;       
+    }
+
+    explode(tetrahedronsAndHalfFaces, halfFace, distance) {
+        const explosion = new Explosion(this, this.__changes.length, tetrahedronsAndHalfFaces, halfFace, distance);
+        this._treatNewChange(explosion);
+        return this;   
+    }
+
+    lay(tetrahedronsAndHalfFaces, absolutePlaneOriginVec3, referenceHalfEdge, targetReferencePositionVec2, targetReferenceOrientationVec2) {
+        const laying = new Laying(this, this.__changes.length, tetrahedronsAndHalfFaces, absolutePlaneOriginVec3, referenceHalfEdge, targetReferencePositionVec2, targetReferenceOrientationVec2);
+        this._treatNewChange(laying);
+        return this;     
+    }
+
+    nextStep(time) {
+        return this.__net.animationStep(time);
+    }
+    
+    _treatNewChange(change) {
+        this.__changes.push(change);
+        const changedHalfFaces = this.__changedHalfFaces;
+        for (const halfFace of change.allHalfFaces()) {
+            if (changedHalfFaces.has(halfFace)) {
+                throw new Error(`HalfFace has more than one change applied to it during single AnimationStep`);
+            }
+            changedHalfFaces.set(halfFace, change);
+        }
+    }
+}
+
+class NetChange {
+
+    get id() {
+        return this.__id;
+    }
+
+    allHalfFaces() {
+        return this.__halfFaceSet.values();
+    }
+    
+    constructor(id, tetrahedronsAndHalfFaces) {
+        this.__id = id;
+        this.__tetrahedronsAndFaces = tetrahedronsAndHalfFaces;
+        const tetrahedronSet = new Set();
+        const halfFaceSet = new Set();
+        for (const tetrahedronOrHalfFace of tetrahedronsAndHalfFaces) {
+            if (tetrahedronOrHalfFace instanceof HalfFace) {
+                halfFaceSet.add(tetrahedronOrHalfFace);
+            } else if (tetrahedronOrHalfFace instanceof Tetrahedron) {
+                tetrahedronSet.add(tetrahedronOrHalfFace);
+            } else {
+                throw new Error(`object is neither Tetrahedron nor HalfFace`);
+            }
+        }
+        for (const tetrahedron of tetrahedronSet) {
+             for (const halfFace of tetrahedron.halfFaces()) {
+                 halfFaceSet.add(halfFace);
+             }   
+        }
+        this.__tetrahedronSet = tetrahedronSet;
+        this.__halfFaceSet = halfFaceSet;
+    }
+}
+
+class Coloring extends NetChange {
+
+    constructor(id, tetrahedronsAndHalfFaces, color) {
+        super(id, tetrahedronsAndHalfFaces);
+        this.__color = color;
+    }
+    
+}
+
+class Explosion extends NetChange {
+
+    constructor(id, halfFace, distance) {
+        super(id);
+        this.__halfFace = halfFace;
+        this.__distance = distance;
+    }
+    
+}
+
+class Laying extends NetChange {
+
+    constructor(id, halfFace, halfEdgeInHalfFace, plane, locationOfSourceVertexInPlane, directionOfHalfEdgeInPlane) {
+        super(id);
+        this.__halfFace = halfFace;
+        this.__halfEdgeInHalfFace = halfEdgeInHalfFace;
+        this.__plane = plane;
+        
+    }
+    
+}
+
+class NetNode {
+
+    constructor(parentNode, tetrahedronsAndHalfFaces) {
+        this.__parentNode = parentNode;
+        const tetrahedronSet = new Set();
+        const halfFaceSet = new Set();
+        for (const tetrahedronOrHalfFace of tetrahedronsAndHalfFaces) {
+            if (tetrahedronOrHalfFace instanceof HalfFace) {
+                halfFaceSet.add(tetrahedronOrHalfFace);
+            } else if (tetrahedronOrHalfFace instanceof Tetrahedron) {
+                tetrahedronSet.add(tetrahedronOrHalfFace);
+            } else {
+                throw new Error(`object is neither Tetrahedron nor HalfFace`);
+            }
+        }
+        for (const tetrahedron of tetrahedronSet) {
+             for (const halfFace of tetrahedron.halfFaces()) {
+                 halfFaceSet.add(halfFace);
+             }   
+        }
+        this.__tetrahedronSet = tetrahedronSet;
+        this.__halfFaceSet = halfFaceSet;
+        this.__colorMap = new Map();
+        this.__unfoldingList = [];
+        this.__transform
+        this.__halfFaceColorMap = new Map();
+        this.__halfFaceUnfoldingMap = new Map();
+    }
+
+    colorHalfFaces(halfFaces, color) {
+        if (!this.__colorMap.has(color)) {
+            this.__color.set(color, new Set());
+        }
+        const coloredHalfFaceSet = this.__colorMap.get(color);
+        for (const halfFace of halfFaces) {
+            coloredHalfFaceSet.add(halfFace);
+        }
+    }
+
+    unfoldHalfFaces(halfFaces, halfEdge, angleInRadians) {
+            
+    }
+
+    _analyseOcclusions() {
+        const occludedHalfFaceSet = new Set();
+        for (const halfFace of halfFaceSet) {
+            const oppositeHalfFace = halfFace.oppositeHalfFace;
+            if (oppositeHalfFace === null) {
+                continue;
+            }
+            const oppositeTetrahedron = oppositeHalfFace.tetrahedron;
+            if (tetrahedronSet.had(oppositeTetrahedron)) {
+                occludedHalfFaceSet.add(halfFace);
+            }
+        }
+        const visibleHalfFaceSet = new Set();
+        for (const halfFace of halfFaceSet) {
+                if (!occludedHalfFaceSet.has(halfFace)) {
+                visibleHalfFaceSet.add(halfFace);
+            }
+        }
+        // TODO
+    }
+}
+    
 export function test(report) {
     report.startSection("test1", "Test 1: Single Gem Mesh", "");
     test1(report);
---
+++ linalg.js
@@ -0,0 +1,400 @@
+export function addV1([a], [b]) {
+       return [a + b];
+}
+
+export function addV2([a1, a2], [b1, b2]) {
+       return [a1 + b1, a2 + b2];
+}
+
+export function addV3([a1, a2, a3], [b1, b2, b3]) {
+       return [a1 + b1, a2 + b2, a3 + b3];
+}
+
+export function addV4([a1, a2, a3, a4], [b1, b2, b3, b4]) {
+       return [a1 + b1, a2 + b2, a3 + b3, a4 + b4];
+}
+
+export function subtractV1([a], [b]) {
+       return [a - b];
+}
+
+export function subtractV2([a1, a2], [b1, b2]) {
+       return [a1 - b1, a2 - b2];
+}
+
+export function subtractV3([a1, a2, a3], [b1, b2, b3]) {
+       return [a1 - b1, a2 - b2, a3 - b3];
+}
+
+export function subtractV4([a1, a2, a3, a4], [b1, b2, b3, b4]) {
+       return [a1 - b1, a2 - b2, a3 - b3, a4 - b4];
+}
+
+export function scaleV1(s, [a]) {
+       return [s * a];
+}
+
+export function scaleV2(s, [a1, a2]) {
+       return [s * a1, s * a2];
+}
+
+export function scaleV3(s, [a1, a2, a3]) {
+       return [s * a1, s * a2, s * a3];
+}
+
+export function scaleV4(s, [a1, a2, a3, a4]) {
+       return [s * a1, s * a2, s * a3, s * a4];
+}
+
+export function multM1V1([a], [b]) {
+       return [
+              a * b,
+       ];
+}
+
+export function multM2V2([a11, a12, a21, a22], [b1, b2]) {
+       return [
+              a11 * b1 + a12 * b2,
+              a21 * b1 + a22 * b2,
+       ];
+}
+
+export function multM3V3([a11, a12, a13, a21, a22, a23, a31, a32, a33], [b1, b2, b3]) {
+       return [
+              a11 * b1 + a12 * b2 + a13 * b3,
+              a21 * b1 + a22 * b2 + a23 * b3,
+              a31 * b1 + a32 * b2 + a33 * b3,
+       ];
+}
+
+export function multM4V4([a11, a12, a13, a14, a21, a22, a23, a24, a31, a32, a33, a34, a41, a42, a43, a44], [b1, b2, b3, b4] {
+       return [
+              a11 * b1 + a12 * b2 + a13 * b3 + a14 * b4,
+              a21 * b1 + a22 * b2 + a23 * b3 + a24 * b4,
+              a31 * b1 + a32 * b2 + a33 * b3 + a34 * b4,
+              a41 * b1 + a42 * b2 + a43 * b3 + a44 * b4,
+       ];             
+}
+
+export function multM1([a], [b]) {
+       return [c];
+}
+
+export function multM2([a11, a12, a21, a22], [b11, b12, b21, b22]) {
+       return [
+              a11 * b11 + a12 * b21,
+              a11 * b12 + a12 * b22,
+              
+              a21 * b11 + a22 * b21,
+              a21 * b12 + a22 * b22,
+      ];
+}
+
+export function multM3([a11, a12, a13, a21, a22, a23, a31, a32, a33], [a11, a12, a13, a21, a22, a23, a31, a32, a33]) {
+       return [
+              a11 * b11 + a12 * b21 + a13 * b31,
+              a11 * b12 + a12 * b22 + a13 * b32,
+              a11 * b13 + a12 * b23 + a13 * b33,
+              
+              a21 * b11 + a22 * b21 + a23 * b31,
+              a21 * b12 + a22 * b22 + a23 * b32,
+              a21 * b13 + a22 * b23 + a23 * b33,
+              
+              a31 * b11 + a32 * b21 + a33 * b31,
+              a31 * b12 + a32 * b22 + a33 * b32,
+              a31 * b13 + a32 * b23 + a33 * b33,
+       ];       
+}
+
+export function multM4([a11, a12, a13, a14, a21, a22, a23, a24, a31, a32, a33, a34, a41, a42, a43, a44],
+       [b11, b12, b13, b14, b21, b22, b23, b24, b31, b32, b33, b34, b41, b42, b43, b44]) {
+       return [
+              a11 * b11 + a12 * b21 + a13 * b31 + a14 * b41,
+              a11 * b12 + a12 * b22 + a13 * b32 + a14 * b42,
+              a11 * b13 + a12 * b23 + a13 * b33 + a14 * b43,
+              a11 * b14 + a12 * b24 + a13 * b34 + a14 * b44,
+
+              a21 * b11 + a22 * b21 + a23 * b31 + a24 * b41,
+              a21 * b12 + a22 * b22 + a23 * b32 + a24 * b42,
+              a21 * b13 + a22 * b23 + a23 * b33 + a24 * b43,
+              a21 * b14 + a22 * b24 + a23 * b34 + a24 * b44,
+
+              a31 * b11 + a32 * b21 + a33 * b31 + a34 * b41,
+              a31 * b12 + a32 * b22 + a33 * b32 + a34 * b42,
+              a31 * b13 + a32 * b23 + a33 * b33 + a34 * b43,
+              a31 * b14 + a32 * b24 + a33 * b34 + a34 * b44,
+              
+              a41 * b11 + a42 * b21 + a43 * b31 + a44 * b41,
+              a41 * b12 + a42_x_b22 + a43 * b32 + a44 * b42,
+              a41 * b13 + a42 * b23 + a43 * b33 + a44 * b43,
+              a41 * b14 + a42 * b24 + a43 * b34 + a44 * b44
+       ];
+}
+
+export function detM1([a]) {
+       return a;
+}
+
+export function detM2([a11, a12, a21, a22]) {
+       return a11 * a22 - a21 * a12;
+}
+
+export function detM3([a11, a12, a13, a21, a22, a23, a31, a32, a33]) {
+       let det = 0;
+       if (a31 !== 0) {
+              det += a31 * detM2([a11, a13, a21, a23]);
+       }
+       if (a32 !== 0) {
+              det -= a32 * detM2([a11, a13, a21, a23]);
+       }
+       if (a33 !== 0) {
+              det += a33 * detM2([a11, a12, a21, a22]);
+       }
+       return det;
+}
+
+export function detM4([a11, a12, a13, a14, a21, a22, a23, a24, a31, a32, a33, a34, a41, a42, a43, a44]) {
+       let det = 0;
+       if (a41 !== 0) {
+              det -= a41 * detM3([a12, a13, a14, a22, a23, a24, a32, a33, a34]);
+       }
+       if (a42 !== 0) {
+              det += a42 * detM3([a11, a13, a14, a21, a23, a24, a31, a33, a34]);
+       }
+       if (a43 !== 0) {
+              det -= a43 * detM3([a11, a12, a14, a21, a22, a24, a31, a32, a34]);
+       }
+       if (a44 !== 0) {
+              det += a44 * detM3([a11, a12, a13, a21, a22, a23, a31, a32, a33]);
+       }
+       return det;
+}
+
+export function composeAffineM1V1([a], [b]) {
+       return [a, b, 
+               0, 1];
+}
+
+export function decomposeAffineM2([a, b, _zero, _one]) {
+       return [[a], [b]];
+}
+
+export function augmentV1([a]) {
+       return [a, 1];
+}
+
+export function deaugmentV2([a, _one]) {
+       return [a];
+}
+
+export function composeAffineM2V2([a11, a12, a21, a22], [b1, b2]) {
+       return [a11, a12, b1,
+               a21, a22, b2, 
+               0, 0, 1];
+}
+
+export function decomposeAffineM3([a11, a12, b1, a21, a22, b2, _zero1, _zero2, _one]) {
+       return [[a11, a12, a13, a21, a22], 
+                              [b1, b2]];
+}
+
+export function augmentV2([a1, a2]) {
+       return [a1, a2, 1];
+}
+
+export function deaugmentV3([a1, a2, _one]) {
+       return [a1, a2];
+}
+
+
+export function composeAffineM3V3([a11, a12, a13, a21, a22, a23, a31, a32, a33], [b1, b2, b3]) {
+       return [a11, a12 a13, b1,
+               a21, a22, a23, b2,
+               a31, a32, a33, b3,
+               0, 0, 0, 1];
+}
+
+export function decomposeAffineM4([a11, a12, a13, b1, a21, a22, a23, b2, a31, a32, a33, b3, _zero1, _zero2, _zero3, _one]) {
+       return [[a11, a12, a13, a21, a22, a23, a31, a32, a33], 
+                              [b1, b2, b3]];
+}
+
+export function augmentV3([a1, a2, a3]) {
+       return [a1, a2, a3, 1];
+}
+
+export function deaugmentV4([a1, a2, a3, _one]) {
+       return [a1, a2, a3];
+}
+
+export function invertM1(m) {
+       const [ a ] = m;
+       
+       // compute determinant
+
+       /* const det11 = 1; */
+       const det = a /* det11 */;
+       
+       if (det === 0) {
+              return null;
+       }
+
+       // implicitly: construct the cofactor matrix
+
+       /*const coM = [ +det11 ];*/
+       
+       // implicitly: transpose the cofactor matrix to get the adjugate
+
+       /*const adjM = [ +det11 ];*/
+
+       // explicitly: perform both steps and multiply simultaneously
+
+       const ted = 1/det;
+
+       const invM = [ ted /* +det11 === 1 */ ]; /* === [ 1/a ] */
+       
+       return invM;
+}
+
+export function invertM2(m) {
+       const [a11, a12, a21, a22 ] = m;
+       
+       // compute submatrix determinants nescessary for the overall determinant
+       
+       const det21 = a12;
+       const det22 = a11;
+       
+       const det = -a21 * det21 + a22 * det22;
+       if (det === 0) {
+              return null;
+       }
+       
+       // compute rest of submatrix determinants
+       
+       const det11 = a22;
+       const det12 = a21;
+
+       // implicitly: construct the cofactor matrix
+
+       /*const coM = [ +det11, -det12,
+                     -det21, +det22, ];*/
+       
+       // implicitly: transpose the cofactor matrix to get the adjugate
+
+       /*const adjM = [ +det11, -det21,
+                     -det12, +det22, ];*/
+
+       // explicitly: perform both steps and multiply simultaneously
+
+       const ted = 1/det;
+
+       const invM = [ ted * +det11, ted * -det21,
+                      ted * -det12, ted * +det22, ];
+       
+       return invM;
+}
+
+export function invertM3(m) {
+       const [a11, a12, a13, a21, a22, a23, a31, a32, a33 ] = m;
+       
+       // compute submatrix determinants nescessary for the overall determinant
+       
+       const det31 = detM2([a12, a13, a22, a23 ]);
+       const det32 = detM2([a11, a13, a21, a23 ]);
+       const det33 = detM2([a11, a12, a21, a22 ]);
+       const det = a31 * det31 - a32 * det32 + a33 * det33;
+       
+       if (det === 0) {
+              return null;
+       }
+       
+       // compute rest of submatrix determinants
+       
+       const det21 = detM2([a12, a13, a32, a33]);
+       const det22 = detM2([a11, a13, a31, a33]);
+       const det23 = detM2([a11, a12, a31, a32]);
+       
+       const det11 = detM2([a22, a23, a32, a33]);
+       const det12 = detM2([a21, a23, a31, a33]);
+       const det13 = detM2([a21, a22, a31, a32]);
+
+       // implicitly: construct the cofactor matrix
+
+       /*const coM = [ +det11, -det12, +det13,
+                     -det21, +det22, -det23,
+                     +det31, -det32, +det33, ];*/
+       
+       // implicitly: transpose the cofactor matrix to get the adjugate
+
+       /*const adjM = [ +det11, -det21, +det31,
+                     -det12, +det22, -det32,
+                     +det13, -det23, +det33, ];*/
+
+       // explicitly: perform both steps and multiply simultaneously
+
+       const ted = 1/det;
+
+       const invM = [ ted * +det11, ted * -det21, ted * +det31,
+                      ted * -det12, ted * +det22, ted * -det32,
+                      ted * +det13, ted * -det23, ted * +det33, ];
+       
+       return invM;
+}
+
+export function invertM4(m) {
+       const [a11, a12, a13, a14, a21, a22, a23, a24, a31, a32, a33, a34, a41, a42, a43, a44] = m;
+       
+       // compute submatrix determinants nescessary for the overall determinant
+       
+       const det41 = detM3([a12, a13, a14, a22, a23, a24, a32, a33, a34]);
+       const det42 = detM3([a11, a13, a14, a21, a23, a24, a31, a33, a34]);
+       const det43 = detM3([a11, a12, a14, a21, a22, a24, a31, a32, a34]);
+       const det44 = detM3([a11, a12, a13, a21, a22, a23, a31, a32, a33]);
+       const det = -a41 * det41 + a42 * det42 - a43 * det43 + a44 * det44;
+       
+       if (det === 0) {
+              return null;
+       }
+       
+       // compute rest of submatrix determinants
+       
+       const det31 = detM3([a12, a13, a14, a22, a23, a24, a42, a43, a44]);
+       const det32 = detM3([a11, a13, a14, a21, a23, a24, a41, a43, a44]);
+       const det33 = detM3([a11, a12, a14, a21, a22, a24, a41, a42, a44]);
+       const det34 = detM3([a11, a12, a13, a21, a22, a23, a41, a42, a43]);
+       
+       const det21 = detM3([a12, a13, a14, a32, a33, a34, a42, a43, a44]);
+       const det22 = detM3([a11, a13, a14, a31, a33, a34, a41, a43, a44]);
+       const det23 = detM3([a11, a12, a14, a31, a32, a34, a41, a42, a44]);
+       const det24 = detM3([a11, a12, a13, a31, a32, a33, a41, a42, a43]);
+       
+       const det11 = detM3([a22, a23, a24, a32, a33, a34, a42, a43, a44]);
+       const det12 = detM3([a21, a23, a24, a31, a33, a34, a41, a43, a44]);
+       const det13 = detM3([a21, a22, a24, a31, a32, a34, a41, a42, a44]);
+       const det14 = detM3([a21, a22, a23, a31, a32, a33, a41, a42, a43]);
+
+       // implicitly: construct the cofactor matrix
+
+       /*const coM = [ +det11, -det12, +det13, -det14,
+                     -det21, +det22, -det23, +det24,
+                     +det31, -det32, +det33, -det34,
+                     -det41, +det42, -det43, +det44 ];*/
+       
+       // implicitly: transpose the cofactor matrix to get the adjugate
+
+       /*const adjM = [ +det11, -det21, +det31, -det41,
+                     -det12, +det22, -det32, +det42,
+                     +det13, -det23, +det33, -det43,
+                     -det14, +det24, -det34, +det44 ];*/
+
+       // explicitly: perform both steps and multiply simultaneously
+
+       const ted = 1/det;
+
+       const invM = [ ted * +det11, ted * -det21, ted * +det31, ted * -det41,
+                      ted * -det12, ted * +det22, ted * -det32, ted * +det42,
+                      ted * +det13, ted * -det23, ted * +det33, ted * -det43,
+                      ted * -det14, ted * +det24, ted * -det34, ted * +det44 ];
+       
+       return invM;
+}
